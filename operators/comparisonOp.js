5711d9bbf088787c993821789b92598018a6f6d7
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-25 16:22:01 +0530

    comparison op

---
+++ operators/comparisonOp.js
@@ -0,0 +1,20 @@
+let num1 = 12;
+let num2 = 13;
+console.log(num1 == num2);
+console.log(num1 != num2);
+
+let num3 = "10";
+let num4 = 10;
+console.log(num3 == num4); //true
+//strict check === 
+console.log(num3 === num4); //false
+//strict check !==
+console.log(num3 !== num4); //true
+
+console.log(num1 > num2); //false
+console.log(num1 < num2); //true
+
+let num5 = 100;
+let num6 = 100;
+console.log(num5 >= num6); //true
+console.log(num5 <= num6); //true
