67656fe34f41968048060dcad84d7adfc521209a
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-25 15:09:54 +0530

    unary op

---
+++ operators/unaryOp.js
@@ -0,0 +1,14 @@
+//unary operators
+let num1 = 5;
+console.log(num1);
+num1++;
+console.log(num1++); //post increment
+console.log(num1);
+console.log(++num1); //pre increment
+let num2 = 8;
+console.log(num2--); //post decrement
+console.log("num2= ", num2);
+
+console.log(--num2); //pre decrement
+
+console.log("num1= ", num1, "num2= ", num2);
