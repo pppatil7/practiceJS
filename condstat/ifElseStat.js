a0ad1beaa75fbdfe901ea573fc11178a0e71b888
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-25 20:02:54 +0530

    if else statement

---
+++ condstat/ifElseStat.js
@@ -0,0 +1,28 @@
+let mode = "light";
+let color;
+
+if (mode === "dark") {
+    color = "black";
+} else {
+    color = "white";
+}
+
+console.log(color);
+
+let age = 21;
+
+if (age >= 18) {
+    console.log("you can vote");
+} else {
+    console.log("you cannot vote");
+}
+
+
+//odd or even
+
+let num = 91;
+if (num % 2 === 0) {
+    console.log("number is even");
+} else {
+    console.log("number is odd");
+}
