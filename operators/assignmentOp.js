60dd113b1089882df94d8fb5ed886c9a69e33d5b
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-25 16:07:47 +0530

    assignment op

---
+++ operators/assignmentOp.js
@@ -0,0 +1,25 @@
+//assignment operators
+let num1 = 23;
+let num2 = 12;
+console.log(num1);
+
+num1 = num2;
+console.log(num1);
+
+num1 += 3; //num1=num1+3
+console.log(num1);
+
+num1 -= 3;
+console.log(num1);
+
+num1 *= 2;
+console.log(num1);
+
+num1 /= 2;
+console.log(num1);
+
+num1 %= 5;
+console.log(num1);
+
+num2 **= 2; //num2^2
+console.log(num2);
