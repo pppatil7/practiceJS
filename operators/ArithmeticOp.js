b2ec73d40e1bd4eed88da8234204f027fd45ac24
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-25 14:57:27 +0530

    arithmetic operators

---
+++ operators/ArithmeticOp.js
@@ -0,0 +1,21 @@
+/* console.log("operators");
+console.log("Hello world!!"); */
+
+//Arithmetic Operators
+let num1 = 12;
+let num2 = 15;
+let addition = num1 + num2;
+
+console.log("addition+ num1= ", addition + num1);
+
+console.log("addition - num1= ", addition - num1);
+
+console.log("num1*2= ", num1 * 2);
+
+console.log("num2/5= ", num2 / 5);
+
+console.log("num1/num2= ", num1 / num2);
+
+console.log("num2%2= ", num2 % 2); //modulus
+
+console.log("num1**2= ", num1 ** 2); //exponentiation
