72cbeda6f6e0930647217a53b422e6d4172df6c2
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-25 13:50:09 +0530

    practice

---
+++ datatypes/practice.js
@@ -0,0 +1,18 @@
+const product = {
+    title: "Book",
+    rating: 4,
+    offer: 20,
+    price: 380
+};
+
+console.log(product);
+
+const profile = {
+    username: "learnJS",
+    isFollow: true,
+    followers: 2560,
+    following: 2
+};
+
+console.log(profile);
+console.log(typeof profile.username);
