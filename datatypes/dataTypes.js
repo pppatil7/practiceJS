08e4aa209add12f8ef71fa55d50e1f7fc0d16404
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-25 13:16:00 +0530

    primitive data types

---
+++ datatypes/dataTypes.js
@@ -0,0 +1,19 @@
+let age = 21; //number
+console.log(age);
+let price = 120.70;
+console.log(price); //
+
+let fullName = "Tony Stark"; //string
+console.log(fullName);
+
+isSubscribed = true; //boolean
+
+let x; //undefined
+
+let y = null;
+console.log(y);
+
+
+let amount = BigInt("29837263"); //bigInt
+
+let z=Symbol("Hello!"); //symbol
