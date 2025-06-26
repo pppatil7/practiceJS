6ec4c60373601bd9bf749d72929f2349ae99e882
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-26 10:10:16 +0530

    for of for in loop

--- loops/forLoop.js
+++ loops/forLoop.js
@@ -11,3 +11,24 @@ for (let count = 1; count <= 10; count++) {
     console.log(number);
     number++;
 }
+
+//for-of loop
+let str = "JavaScript";
+
+for (let i of str) {
+    console.log(i);
+}
+
+//for-in loop
+const student = {
+    fullName: "peter will",
+    age: 21,
+    cgpa: 9.5,
+    isPass: true
+};
+
+for (let key in student) {
+    console.log(key,":", student[key]);
+}
+
+
