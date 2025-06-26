27dea2ee625056b3f65a4fa9120a529af42785b5
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-26 09:55:30 +0530

    while do while loop

---
+++ loops/whileLoop.js
@@ -0,0 +1,17 @@
+let i = 1;
+while (i <= 5) {
+    console.log("i=", i);
+    i++;
+}
+
+let num = 40;
+
+/* while (num < 30) {
+    console.log("Helo!!");
+} */
+
+//do while
+
+do {
+    console.log("Helo!!");
+} while (num < 30);
