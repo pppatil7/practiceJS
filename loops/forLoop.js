936bd626ccaf99624c4a27a47d3a2ae58413fcb6
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-26 09:47:46 +0530

    for loop

---
+++ loops/forLoop.js
@@ -0,0 +1,13 @@
+for (let count = 0; count < 5; count++) {
+    console.log("Helo!!");
+}
+
+console.log("loop has ended");
+
+
+//print 1 to 10;
+let number = 1;
+for (let count = 1; count <= 10; count++) {
+    console.log(number);
+    number++;
+}
