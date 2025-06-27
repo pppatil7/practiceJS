31c2f348b426f5be0f530bd8c6db1cdf64c76484
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-27 13:32:57 +0530

    dom

---
+++ dom/script.js
@@ -0,0 +1,9 @@
+// console.log(window.document.children);
+
+let headingId = document.getElementById("headingId");
+console.dir(headingId);
+console.log(headingId);
+
+let divClass = document.getElementsByClassName("divClass");
+console.dir(divClass);
+console.log(divClass);
