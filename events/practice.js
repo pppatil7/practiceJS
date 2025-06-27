26d7097804b2deaa95f5b03dc294d7a70f9f2992
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-27 21:02:23 +0530

    practice problem

--- events/practice.js
+++ events/practice.js
@@ -1,7 +1,24 @@
 let modeBtn = document.querySelector("#mode");
+let body = document.querySelector("body");
+let currMode = "light";
+
 
 const changeMode = () => {
-    console.log("this function is to change the mode");
+    // console.log("this function is to change the mode");
+    if (currMode === "light") {
+        currMode = "dark";
+        // document.querySelector("body").classList.add("dark");
+        body.classList.add("dark");
+        body.classList.remove("light");
+
+    } else {
+        currMode = "light";
+        // document.querySelector("body").classList.add("white");
+        body.classList.add("light");
+        body.classList.remove("dark");
+
+    }
+    console.log(currMode);
 };
 
 modeBtn.addEventListener("click", changeMode);
